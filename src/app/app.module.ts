4e4e5d5290cf9237a87217aae9a10d5eb024ed0b
Thomas <thomas@icdesign.com.au>
2018-04-29 18:12:31 +1000

    Bugsnag + Bugsnag-angular for replicating issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import bugsnag-js and bugsnag-angular
+import BugsnagErrorHandler from 'bugsnag-angular'
+import bugsnag from 'bugsnag-js'
+
+// configure Bugsnag ASAP, before any other imports
+const bugsnagClient = bugsnag('API_KEY')
+
+// create a factory which will return the bugsnag error handler
+export function errorHandlerFactory() {
+  return new BugsnagErrorHandler(bugsnagClient)
+}
+
 import { AppComponent } from './app.component';
 
 @NgModule({
